4c7fa0d75dc049eb71da99fa1bc45bed842a72fe
kestonbhola <kbhola001@sgu.edu>
2026-02-16 00:15:32 -0400

    BugFix

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 # Class Project
 This repository is used for teaching students how to use Git and GitHub.
-=======
-# Class Project
-This repository is used for teaching students how to use Git and GitHub.
->>>>>>> ba181f9e27090c6f9f176b96277a5869b162c384
--- sguscript.js
+++ sguscript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var acc = document.getElementsByClassName("accordion");
         for (var i = 0; i < acc.length; i++) {
             acc[i].addEventListener("click", function() {
@@ -11,18 +10,4 @@ var acc = document.getElementsByClassName("accordion");
                 }
             });
         }
-=======
-var acc = document.getElementsByClassName("accordion");
-        for (var i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var panel = this.nextElementSibling;
-                if (panel.style.display === "block") {
-                    panel.style.display = "none";
-                } else {
-                    panel.style.display = "block";
-                }
-            });
-        }
->>>>>>> ba181f9e27090c6f9f176b96277a5869b162c384
 		
